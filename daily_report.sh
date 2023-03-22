ab874519ceb37cd7728355d52f1199ad4c9e135f
purplewish07 <70552611+purplewish07@users.noreply.github.com>
2023-03-22 09:09:48 +0800

    Update daily_report.sh

--- daily_report.sh
+++ daily_report.sh
@@ -21,6 +21,7 @@ echo $udate
 from=$((udate+28800))
 to=$((from+86399))
 
+GF_SCRIPT="/home/uta_iot/github_repo/grafana-generate-pdf-nodejs/grafana_A3.js"
 echo $from , $to
 GF_TIME="&from=${from}000&to=${to}000"
 echo $GF_TIME
@@ -29,9 +30,9 @@ GF_USER="admin"
 GF_PASSWORD="admin"
 
 # 生產數量
-OUTPUT_PDF="./grafana_output/生產統計表/${vdate}.pdf"
+OUTPUT_PDF="/home/uta_iot/grafana_output/生產統計表/${vdate}.pdf"
 GF_DASH_URL="https://192.168.2.3:3000/d/_Mjk-mmgk/mei-ri-chan-chu-tong-ji-biao-ge-2_you-ban-bie?orgId=2"
-node grafana_A3.js "$GF_DASH_URL$GF_TIME" $GF_USER:$GF_PASSWORD $OUTPUT_PDF
+node $GF_SCRIPT "$GF_DASH_URL$GF_TIME" $GF_USER:$GF_PASSWORD $OUTPUT_PDF
 
 # machine
 OUTPUT_PDF="./grafana_output/機台稼動明細/${vdate}"
@@ -41,9 +42,9 @@ mkdir -p $OUTPUT_PDF
 #node grafana_A3.js "$GF_DASH_URL$GF_TIME" $GF_USER:$GF_PASSWORD $OUTPUT_PDF
 for str in ${machine[@]}; do
   GF_DASH_URL="https://192.168.2.3:3000/d/nLFCnaWgk/report_single?orgId=2&var-machine=${str}"
-  OUTPUT_PDF="./grafana_output/機台稼動明細/${vdate}/${str}.pdf"
+  OUTPUT_PDF="/home/uta_iot/grafana_output/機台稼動明細/${vdate}/${str}.pdf"
   echo "$GF_DASH_URL$GF_TIME" $GF_USER:$GF_PASSWORD $OUTPUT_PDF
-  node grafana_A3.js "$GF_DASH_URL$GF_TIME" $GF_USER:$GF_PASSWORD $OUTPUT_PDF
+  node $GF_SCRIPT "$GF_DASH_URL$GF_TIME" $GF_USER:$GF_PASSWORD $OUTPUT_PDF
 
   sleep 120
 done
