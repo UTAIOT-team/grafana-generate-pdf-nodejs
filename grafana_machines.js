9f008468699d851cdd12f144232e781a602ae942
purplewish07 <70552611+purplewish07@users.noreply.github.com>
2024-05-14 10:00:47 +0800

    Update grafana_machines.js

--- grafana_machines.js
+++ grafana_machines.js
@@ -51,108 +51,114 @@ const auth_header = 'Basic ' + new Buffer.from(auth_string).toString('base64');
       ignoreHTTPSErrors: true
     });
 
-    // 使用 Promise.all 来并行处理所有页面
-    const pdfResults = await Promise.all(urlsAndPaths.map(async ({ url, pdfPath }) => {
-    
-      const page = await browser.newPage();
+    for (let i = 0; i < urlsAndPaths.length; i += 5) {
+      const batch = urlsAndPaths.slice(i, i + 5);
 
-      // Set basic auth headers
-      await page.setExtraHTTPHeaders({ 'Authorization': auth_header });
-
-      // Increase timeout from the default of 30 seconds to 120 seconds, to allow for slow-loading panels
-      await page.setDefaultNavigationTimeout(120000);
-
-      // Increasing the deviceScaleFactor gets a higher-resolution image. The width should be set to
-      // the same value as in page.pdf() below. The height is not important
-      await page.setViewport({
-        width: width_px,
-        height: 2480,
-      //   height: 2500,
-        deviceScaleFactor: 1,
-        isMobile: false
-      })
-
-      // Wait until all network connections are closed (and none are opened withing 0.5s).
-      // In some cases it may be appropriate to change this to {waitUntil: 'networkidle2'},
-      // which stops when there are only 2 or fewer connections remaining.
-      await page.goto(url, { waitUntil: 'networkidle0' });
-
-      // Hide all panel description (top-left "i") pop-up handles and, all panel resize handles
-      // Annoyingly, it seems you can't concatenate the two object collections into one
-      await page.evaluate(() => {
-        let infoCorners = document.getElementsByClassName('panel-info-corner');
-        for (el of infoCorners) { el.hidden = true; };
-        let resizeHandles = document.getElementsByClassName('react-resizable-handle');
-        for (el of resizeHandles) { el.hidden = true; };
-      });
-
-      // // Get the height of the main canvas, and add a margin
-      // var height_px = await page.evaluate(() => {
-      //   return document.getElementsByClassName('react-grid-layout')[0].getBoundingClientRect().bottom;
-      // }) + 20;
-
-      // // == auto scroll to the bottom to solve long grafana dashboard start
-      // async function autoScroll(page) {
-      //   await page.evaluate(async () => {
-      //     await new Promise((resolve, reject) => {
-      //       var totalHeight = 0;
-      //       var distance = 100;
-      //       var height_px = document.getElementsByClassName('react-grid-layout')[0].getBoundingClientRect().bottom;
-      //       var timer = setInterval(() => {
-      //         var scrollHeight = height_px;
-
-      //         // select the scrollable view
-      //         // in newer version of grafana the scrollable div is 'scrollbar-view'
-      //         var scrollableEl = document.querySelector('.view') || document.querySelector('.scrollbar-view');
-      //         // element.scrollBy(0, distance);
-      //         scrollableEl.scrollBy({
-      //           top: distance,
-      //           left: 0,
-      //           behavior: 'smooth'
-      //         });
-
-      //         totalHeight += distance;
-
-      //         console.log('totalHeight', totalHeight)
-
-      //         if (totalHeight >= scrollHeight) {
-      //           clearInterval(timer);
-      //           resolve();
-      //         }
-      //       }, 300);
-      //     });
-      //   });
-      // }
-
-      // await autoScroll(page);
-      // // == auto scroll to the bottom to solve long grafana dashboard end
+      // 使用 Promise.all 来并行处理所有页面
+      await Promise.all(batch.map(async ({ url, pdfPath }) => {
       
-      
-      await page.pdf({
-        path: pdfPath,
-        printBackground: true,
-      //   width: width_px + 'px',
-      //   height: height_px + 'px',
-        width: 1754 + 'px',
-        height: 3480 + 'px',
-        //    format: 'Letter', <-- see note above for generating "paper-sized" outputs
-      //   format: 'A3',
-        scale: 1,
-        displayHeaderFooter: false,
-        margin: {
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-        },
-      });
+        const page = await browser.newPage();
+        try{
 
-      await page.close();
-      return pdfPath;
-    }));
+          // Set basic auth headers
+          await page.setExtraHTTPHeaders({ 'Authorization': auth_header });
 
-    await browser.close();
+          // Increase timeout from the default of 30 seconds to 120 seconds, to allow for slow-loading panels
+          await page.setDefaultNavigationTimeout(120000);
+
+          // Increasing the deviceScaleFactor gets a higher-resolution image. The width should be set to
+          // the same value as in page.pdf() below. The height is not important
+          await page.setViewport({
+            width: width_px,
+            height: 2480,
+          //   height: 2500,
+            deviceScaleFactor: 1,
+            isMobile: false
+          });
+
+          // Wait until all network connections are closed (and none are opened withing 0.5s).
+          // In some cases it may be appropriate to change this to {waitUntil: 'networkidle2'},
+          // which stops when there are only 2 or fewer connections remaining.
+          await page.goto(url, { waitUntil: 'networkidle0' });
+
+          // Hide all panel description (top-left "i") pop-up handles and, all panel resize handles
+          // Annoyingly, it seems you can't concatenate the two object collections into one
+          await page.evaluate(() => {
+            let infoCorners = document.getElementsByClassName('panel-info-corner');
+            for (el of infoCorners) { el.hidden = true; };
+            let resizeHandles = document.getElementsByClassName('react-resizable-handle');
+            for (el of resizeHandles) { el.hidden = true; };
+          });
+
+          // // Get the height of the main canvas, and add a margin
+          // var height_px = await page.evaluate(() => {
+          //   return document.getElementsByClassName('react-grid-layout')[0].getBoundingClientRect().bottom;
+          // }) + 20;
+
+          // // == auto scroll to the bottom to solve long grafana dashboard start
+          // async function autoScroll(page) {
+          //   await page.evaluate(async () => {
+          //     await new Promise((resolve, reject) => {
+          //       var totalHeight = 0;
+          //       var distance = 100;
+          //       var height_px = document.getElementsByClassName('react-grid-layout')[0].getBoundingClientRect().bottom;
+          //       var timer = setInterval(() => {
+          //         var scrollHeight = height_px;
+
+          //         // select the scrollable view
+          //         // in newer version of grafana the scrollable div is 'scrollbar-view'
+          //         var scrollableEl = document.querySelector('.view') || document.querySelector('.scrollbar-view');
+          //         // element.scrollBy(0, distance);
+          //         scrollableEl.scrollBy({
+          //           top: distance,
+          //           left: 0,
+          //           behavior: 'smooth'
+          //         });
+
+          //         totalHeight += distance;
+
+          //         console.log('totalHeight', totalHeight)
+
+          //         if (totalHeight >= scrollHeight) {
+          //           clearInterval(timer);
+          //           resolve();
+          //         }
+          //       }, 300);
+          //     });
+          //   });
+          // }
+
+          // await autoScroll(page);
+          // // == auto scroll to the bottom to solve long grafana dashboard end
+          
+          
+          await page.pdf({
+            path: pdfPath,
+            printBackground: true,
+          //   width: width_px + 'px',
+          //   height: height_px + 'px',
+            width: 1754 + 'px',
+            height: 3480 + 'px',
+            //    format: 'Letter', <-- see note above for generating "paper-sized" outputs
+          //   format: 'A3',
+            scale: 1,
+            displayHeaderFooter: false,
+            margin: {
+              top: 0,
+              right: 0,
+              bottom: 0,
+              left: 0,
+            },
+          });
+
+        } finally {
+          await page.close();
+        }
+      }));
+    }
   } catch (error) {
-    console.log(error);
+    console.error('An error occurred:', error);
+  } finally {
+    await browser.close();
   }
 })();
